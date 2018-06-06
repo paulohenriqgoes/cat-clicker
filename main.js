188bac6c704db7b3f474e7e24d6dacb23be5811c
pauloH <paulo.hgf1408@gmail.com>
2018-06-06 00:29:19 +0000

    ADD: Utilizar dois gatos no cat clicker

--- main.js
+++ main.js
@@ -1,17 +1,54 @@
 (function () {
-  var heart, clicks, increment;
+
+  var cats = [
+    {
+      id: '#cat1',
+      increment: 0,
+      name: 'Kawaii cat',
+      message: 'Este gato é muito fofo, mas é muito fofo tão fofo que da vontade de aperta',
+      image: './images/cat.jpg'
+    },
+    {
+      id: '#cat2',
+      increment: 0,
+      name: 'Snake cat',
+      message: 'Este gato é muito astuto, vive pelas sombras e está a onde vc menos espera',
+      image: './images/cat2.jpg'
+    }
+  ];
 
   function init() {
-    increment = 0;
-    heart = document.querySelector('#like-cat');
-    clicks = document.querySelector('.clicks');
+    // increment = 0;
+    // heart = document.querySelector('#like-cat');
+    // clicks = document.querySelector('.clicks');
 
-    heart.addEventListener('click', function (e) {
-      e.preventDefault();
+    // heart.addEventListener('click', function (e) {
+    //   e.preventDefault();
 
-      increment += 1;
-      clicks.innerText  = '' + increment;
-    }, false);
+    //   increment += 1;
+    //   clicks.innerText  = '' + increment;
+    // }, false);
+
+    cats.forEach(function(cat) {
+      var card, title, message, button, clicks;
+
+      card = document.querySelector(cat.id);
+      title = document.querySelector(cat.id + ' .card-title');
+      message = document.querySelector(cat.id + ' .card-message');
+      button = document.querySelector(cat.id + ' button');
+      clicks = document.querySelector(cat.id + ' .clicks');
+
+      card.style.backgroundImage = 'url(' + cat.image + ')';
+
+      title.innerText = cat.name;
+      message.innerText = cat.message;
+      button.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        cat.increment += 1;
+        clicks.innerText  = '' + cat.increment;
+      }, false);
+    });
   }
 
   document.onreadystatechange = function() {
